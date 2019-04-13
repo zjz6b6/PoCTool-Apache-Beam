9b191fdda7456ce62f2744ed1214bbe666d1368c
Hussein <tejanahmedhu@gmail.com>
2019-04-14 00:19:47 +0100

    accessing some information from input (csv file) although got an error

--- comparison-beam/src/main/java/org/apache/beam/MinimalWordComparator.java
+++ comparison-beam/src/main/java/org/apache/beam/MinimalWordComparator.java
@@ -18,7 +18,6 @@
 package org.apache.beam.examples;
 
 import java.util.Arrays;
-import java.io.File;
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.io.TextIO;
 import org.apache.beam.sdk.options.PipelineOptions;
@@ -29,9 +28,25 @@ import org.apache.beam.sdk.transforms.FlatMapElements;
 import org.apache.beam.sdk.transforms.MapElements;
 import org.apache.beam.sdk.values.KV;
 import org.apache.beam.sdk.values.TypeDescriptors;
-import org.apache.beam.sdk.io.Read;
-import org.apache.beam.sdk.testing.TestPipeline;
+import org.apache.beam.sdk.io.FileIO;
+import java.io.Reader;
+import java.io.Serializable;
+import java.sql.Types;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.lang.Integer;
+import java.nio.charset.StandardCharsets;
+import java.lang.Double;
+import java.util.List;
+import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.transforms.SerializableFunction;
+import org.apache.beam.sdk.transforms.DoFn.ProcessContext;
+import org.apache.beam.sdk.transforms.DoFn.ProcessElement;
+import static java.nio.charset.StandardCharsets.UTF_8;
 import org.apache.beam.sdk.values.PCollection;
+
 
 /**
  * An example that counts words in Shakespeare.
@@ -62,29 +77,13 @@ import org.apache.beam.sdk.values.PCollection;
 public class MinimalWordComparator {
 
 
-  public void test_reading() throws Exception {
-    final File file =
-            new File(getClass().getResource("files/credit_train_data.csv").toURI());
-
-    final Pipeline pipeline = TestPipeline.create();
-
-    final PCollection<String> output =
-            pipeline.apply(Read.from(CsvWithHeaderFileSource.from(file.getAbsolutePath())));
-
-
-           pipeline.apply(TextIO.write().to("output"));
-
-    pipeline.run();
-    pipeline.run().waitUntilFinish();
-}
-
   public static void main(String[] args) {
-
+       
     // Create a PipelineOptions object. This object lets us set various execution
     // options for our pipeline, such as the runner you wish to use. This example
     // will run with the DirectRunner by default, based on the class path configured
     // in its dependencies.
-    // PipelineOptions options = PipelineOptionsFactory.create();
+    PipelineOptions options = PipelineOptionsFactory.create();
 
     // In order to run your pipeline, you need to make following runner specific changes:
     //
@@ -102,45 +101,45 @@ public class MinimalWordComparator {
     //      .setRunner(FlinkRunner.class);
 
     // Create the Pipeline object with the options we defined above
-    // Pipeline p = Pipeline.create(options);
+    Pipeline p = Pipeline.create(options);
 
     // Concept #1: Apply a root transform to the pipeline; in this case, TextIO.Read to read a set
     // of input text files. TextIO.Read returns a PCollection where each element is one line from
     // the input text (a set of Shakespeare's texts).
 
     // This example reads a public data set consisting of the complete works of Shakespeare.
-    // PCollection<String> data1 = p.apply(TextIO.read().from("files/credit_train_data.csv"))
+     p.apply(TextIO.read().from("files/credit_train_data.csv"))
+      .apply(TextIO.readAll())
                                 
+                              
         // Concept #2: Apply a FlatMapElements transform the PCollection of text lines.
         // This transform splits the lines in PCollection<String>, where each element is an
         // individual word in Shakespeare's collected texts.
-        // .apply(
-        //     FlatMapElements.into(TypeDescriptors.strings())
-        //         .via((String word) -> Arrays.asList(word.split("[^\\p{L}]+"))))
+        .apply(
+            FlatMapElements.into(TypeDescriptors.strings())
+                .via((String word) -> Arrays.asList(word.split("[^\\p{L}]+"))))
         // We use a Filter transform to avoid empty word
-        // .apply(Filter.by((String word) -> !word.isEmpty()))
+        .apply(Filter.by((String word) -> !word.isEmpty()))
         // Concept #3: Apply the Count transform to our PCollection of individual words. The Count
         // transform returns a new PCollection of key/value pairs, where each key represents a
         // unique word in the text. The associated value is the occurrence count for that word.
-        // .apply(Count.perElement())
+        .apply(Count.perElement())
         // Apply a MapElements transform that formats our PCollection of word counts into a
         // printable string, suitable for writing to an output file.
-        // .apply(
-        //     MapElements.into(TypeDescriptors.strings())
-        //         .via(
-        //             (KV<String, Long> wordCount) ->
-        //                 wordCount.getKey() + ": " + wordCount.getValue()))
+        .apply(
+            MapElements.into(TypeDescriptors.strings())
+                .via(
+                    (KV<String, Long> wordCount) ->
+                        wordCount.getKey() + ": " + wordCount.getValue()))
         // Concept #4: Apply a write transform, TextIO.Write, at the end of the pipeline.
         // TextIO.Write writes the contents of a PCollection (in this case, our PCollection of
         // formatted strings) to a series of text files.
         //
         // By default, it will write to a set of files with names like wordcounts-00001-of-00005
-        // .apply(TextIO.write().to("wordcounts"));
+        .apply(TextIO.write().to("wordcounts"));
 
-    // p.run().waitUntilFinish();
-    
-    MinimalWordComparator obj = new MinimalWordComparator();
-    
-    obj.test_reading();
+            p.run().waitUntilFinish();
+
+
   }
 }
