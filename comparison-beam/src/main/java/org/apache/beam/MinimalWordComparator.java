83bb754911ecb9f5252dd0eccb1f9879115a9271
Hussein <tejanahmedhu@gmail.com>
2019-04-16 23:56:50 +0100

    first commit in new branch

--- comparison-beam/src/main/java/org/apache/beam/MinimalWordComparator.java
+++ comparison-beam/src/main/java/org/apache/beam/MinimalWordComparator.java
@@ -1,56 +1,79 @@
 package org.apache.beam;
 
+import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.Arrays;
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.io.TextIO;
 import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.transforms.Count;
+import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.transforms.Filter;
 import org.apache.beam.sdk.transforms.FlatMapElements;
 import org.apache.beam.sdk.transforms.MapElements;
+import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.transforms.DoFn.ProcessContext;
+import org.apache.beam.sdk.transforms.DoFn.ProcessElement;
 import org.apache.beam.sdk.values.KV;
 import org.apache.beam.sdk.values.TypeDescriptors;
+
+import com.opencsv.CSVReader;
+
 import org.apache.beam.sdk.values.PCollection;
 
 
 public class MinimalWordComparator {
+	
+	
 
-  static boolean areHeadersEqual = false;
-
+//  static boolean areHeadersEqual = false;
+  
   public static void main(String[] args) {
+	  
+//    PipelineOptions options = PipelineOptionsFactory.create();
+//    Pipeline p = Pipeline.create(options);
 
-    PipelineOptions options = PipelineOptionsFactory.create();
-    Pipeline p = Pipeline.create(options);
 
-    //Read from all files in the files/ directory
-    p.apply(TextIO.read().from("files/*"))
-	//Filter by only lines that aren't empty and do not contain "-".
-	//Assuming header lines will never contain "-", and non header lines will always contain "-".
-        .apply(Filter.by((String line) -> !line.isEmpty() && !line.contains("-")))
-        //Count the frequency of the header lines.
-        .apply(Count.perElement())
-        //Transform the result into a printable String, if we counted more than one of the same header lines then we know the headers are equal.
-        .apply(
-            MapElements.into(TypeDescriptors.strings())
-                .via(
-                    (KV<String, Long> lineCount) ->
-                        "Headers are equal" + "? " + (areHeadersEqual = (lineCount.getValue() > 1)) ));
-        
-        // Apply a write transform, TextIO.Write, at the end of the pipeline.
-        // TextIO.Write writes the contents of a PCollection (in this case, our PCollection of
-        // formatted strings) to a series of text files.
-        //
-        // By default, it will write to a set of files with names like headerequality-00001-of-00005
-//        .apply(TextIO.write().to("headerequality"));
-
-    p.run().waitUntilFinish();
+//    p.run().waitUntilFinish();
     
-    if(areHeadersEqual){
-        System.out.println("HEADERS ARE EQUAL, CONTINUE WITH PROCESSING");
-	//We can probably start a new process here using p.apply() since we know headers are equal
-    }else{
-        System.out.println("HEADERS ARE NOT EQUAL, STOPPING");
-    }
+    //Read from all files in the files/ directory
+
+	  try{
+		  	//Read file from files/ directory
+			 CSVReader reader = new CSVReader(new FileReader("files/credit_train_data.csv"));
+			 String[] nextLine;
+			 ArrayList<String> rows = new ArrayList<>();
+			 //Iterate through each line printing its contents			 
+			 while((nextLine = reader.readNext()) != null){
+				 if(nextLine != null){
+					 rows.add(Arrays.toString(nextLine));
+					 System.out.println(Arrays.toString(nextLine));		  
+				 }
+			 }
+
+			 
+			//Split the lines based on ',' appearing [Loan ID Customer ID Loan Status Current Loan Amount Term Credit Score]		 
+			String HEADERS_SPLIT[] = rows.get(0).split(",");
+			ArrayList<String>HEADERS = new ArrayList<>();		
+			//Iterate adding the HEADERS	
+			for (int i = 0; i < HEADERS_SPLIT.length; i++) {
+				HEADERS.add(HEADERS_SPLIT[i]);
+			}
+			//Iterate printing the HEADERS
+			for (int i = 0; i < HEADERS.size(); i++) {
+				System.out.println(HEADERS.get(i));
+			}	
+			
+				//Iterate printing the second header
+				System.out.println(HEADERS.get(1));
+			
+		}catch(Exception e){
+			System.out.println(e);
+		}
+
+  
+   
   }
 }
+		  
